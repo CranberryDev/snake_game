70138472aab3529e7f0dfe86fc8e0451f44b446d
bootcamp <Agaev.A.R@sberbank.ru>
2019-09-14 22:33:32 +0300

    init commit

---
+++ Snake/GameManager.swift
@@ -0,0 +1,185 @@
+//
+//  GameManager.swift
+//  Snake
+//
+//  Created by Artem Agaev on 14/09/2019.
+//  Copyright © 2019 Artem Agaev. All rights reserved.
+//
+
+import SpriteKit
+
+class GameManager {
+    
+    var scene: GameScene!
+    
+    var nextTimeToPrint: Double?
+    var timeExtensions: Double = 0.15
+    var playerDirection: Int = 4 // 1 == left, 2 == up, 3 == right, 4 == down
+    
+    var currentScore: Int = 0
+    
+    
+    init(scene: GameScene) {
+        self.scene = scene
+    }
+    
+    func initGame() {
+        scene.playerPosition.append((10, 10))
+        scene.playerPosition.append((10, 11))
+        scene.playerPosition.append((10, 12))
+        renderChanges()
+        
+        generateRandomPoint()
+    }
+    
+    func update(time: Double) {
+        if nextTimeToPrint == nil {
+            nextTimeToPrint = time + timeExtensions
+        } else if time >= nextTimeToPrint! {
+            nextTimeToPrint = time + timeExtensions
+            updatePlayerPosition()
+            
+            checkForScore()
+            checkForDeath()
+        }
+    }
+    
+    func renderChanges() {
+        for (node, x, y) in scene.gameArray {
+            if contains(a: scene.playerPosition, v: (x, y)) {
+                node.fillColor = SKColor.cyan
+            } else {
+                node.fillColor = SKColor.clear
+                
+                if scene.scorePos != nil {
+                    //Pay attention x == y and y == x (not x == x and y == y)
+                    if Int((scene.scorePos?.x)!) == y && Int((scene.scorePos?.y)!) == x {
+                        node.fillColor = SKColor.red
+                    }
+                }
+            }
+        }
+    }
+    
+    func contains(a: [(Int, Int)], v: (Int, Int)) -> Bool {
+        let (c1, c2) = v
+        for (pX, pY) in a {
+            if c1 == pX && c2 == pY {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func swipe(ID: Int) {
+        if !(playerDirection == 4 && ID == 2) && !(playerDirection == 2 && ID == 2) {
+            if !(playerDirection == 1 && ID == 3) && !(playerDirection == 3 && ID == 1) {
+                if playerDirection != 0 {
+                    playerDirection = ID
+                }
+            }
+        }
+    }
+    
+    //MARK: Private methods
+    
+    //Collision detection
+    private func checkForScore() {
+        if scene.scorePos != nil {
+            let x = scene.playerPosition[0].0
+            let y = scene.playerPosition[0].1
+            
+            if Int((scene.scorePos?.x)!) == y && Int((scene.scorePos?.y)!) == x {
+                currentScore += 1
+                scene.currentScore.text = "Score: \(currentScore)"
+                generateRandomPoint()
+                
+                scene.playerPosition.append(scene.playerPosition.last!)
+                scene.playerPosition.append(scene.playerPosition.last!)
+                scene.playerPosition.append(scene.playerPosition.last!)
+            }
+        }
+    }
+    
+    private func checkForDeath() {
+        if scene.playerPosition.count > 0 {
+            var arrayOfPosition = scene.playerPosition
+            let headOfSnake = arrayOfPosition[0]
+            arrayOfPosition.remove(at: 0)
+            if contains(a: arrayOfPosition, v: headOfSnake) {
+                playerDirection = 0
+            }
+            
+        }
+    }
+    
+    private func updatePlayerPosition() {
+        var xChange = -1
+        var yChange = 0
+        
+        switch playerDirection {
+        case 1:
+            //left
+            xChange = -1
+            yChange = 0
+        case 2:
+            //up
+            xChange = 0
+            yChange = -1
+        case 3:
+            //right
+            xChange = 1
+            yChange = 0
+        case 4:
+            //down
+            xChange = 0
+            yChange = 1
+        case 0:
+            //dead
+            xChange = 0
+            yChange = 0
+            break;
+        default:
+            break;
+        }
+        
+        if scene.playerPosition.count > 0 {
+            var start = scene.playerPosition.count - 1
+            while start > 0 {
+                scene.playerPosition[start] = scene.playerPosition[start - 1]
+                start -= 1
+            }
+            scene.playerPosition[0] = (scene.playerPosition[0].0 + yChange, scene.playerPosition[0].1 + xChange)
+        }
+        
+        if scene.playerPosition.count > 0 {
+            let x = scene.playerPosition[0].1
+            let y = scene.playerPosition[0].0
+            if y > 39 {
+                scene.playerPosition[0].0 = 0
+            } else if y < 0 {
+                scene.playerPosition[0].0 = 39
+            } else if x > 19 {
+                scene.playerPosition[0].1 = 0
+            } else if x < 0 {
+                scene.playerPosition[0].1 = 19
+            }
+        }
+        renderChanges()
+    }
+    
+    private func generateRandomPoint() {
+        var randomX = CGFloat(arc4random_uniform(19))
+        var randomY = CGFloat(arc4random_uniform(39))
+        
+        //Regenerate new point if it generated on player body
+        while contains(a: scene.playerPosition, v: (Int(randomX), Int(randomY))) {
+            randomX = CGFloat(arc4random_uniform(19))
+            randomY = CGFloat(arc4random_uniform(39))
+        }
+        //X is actually randomY and Y is randomX, no ideas why
+        scene.scorePos = CGPoint(x: randomX, y: randomY)
+        ;
+    }
+    
+}
--- Snake/GameScene.swift
+++ Snake/GameScene.swift
@@ -11,79 +11,196 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    //MARK: Main menu properties
+    
+    var gameLogo: SKLabelNode!
+    var bestScore: SKLabelNode!
+    var playButton: SKShapeNode!
+    
+    //MARK: Game properties
+    
+    var currentScore: SKLabelNode!
+    var playerPosition: [(Int, Int)] = []
+    var gameBG: SKShapeNode!
+    var gameArray: [(node: SKShapeNode, x: Int, y: Int)] = []
+    
+    var scorePos:CGPoint?
+    
+    var game: GameManager!
+    
     
     override func didMove(to view: SKView) {
+        initMenu()
+        game = GameManager.init(scene: self)
+        initGameView()
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        //Register touch listeners
+        let swipeRight:UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipeR))
+        swipeRight.direction = .right
+        view.addGestureRecognizer(swipeRight)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        let swipeLeft:UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipeL))
+        swipeLeft.direction = .left
+        view.addGestureRecognizer(swipeLeft)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        let swipeUp:UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipeU))
+        swipeUp.direction = .up
+        view.addGestureRecognizer(swipeUp)
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        let swipeDown:UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipeD))
+        swipeDown.direction = .down
+        view.addGestureRecognizer(swipeDown)
+        
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
+
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        game.update(time: currentTime)
     }
+    
+    //MARK: Event listeners
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            let touchedNode = self.nodes(at: location)
+            for node in touchedNode {
+                if node.name == "play_button" {
+                    startGame()
+                }
+            }
+        }
+    }
+    
+    //MARK: Swipe methods
+    
+    @objc func swipeR() {
+        game.swipe(ID: 3)
+    }
+    
+    @objc func swipeL() {
+        game.swipe(ID: 1)
+    }
+    
+    @objc func swipeU() {
+        game.swipe(ID: 2)
+    }
+    
+    @objc func swipeD() {
+        game.swipe(ID: 4)
+    }
+    
+    
+    //MARK: Private methods
+    
+    private func initMenu() {
+        //Create game title
+        gameLogo = SKLabelNode(fontNamed: "ArialRoundedMTBold")
+        gameLogo.zPosition = 1
+        gameLogo.position = CGPoint(x: 0, y: (frame.size.height / 2) - 200)
+        gameLogo.fontSize = 60
+        gameLogo.text = "Snake"
+        gameLogo.fontColor = SKColor.red
+        self.addChild(gameLogo)
+        
+        //Create best score label
+        bestScore = SKLabelNode(fontNamed: "ArialRoundedMTBold")
+        bestScore.zPosition = 1
+        bestScore.position = CGPoint(x: 0, y: gameLogo.position.y - 50)
+        bestScore.fontSize = 40
+        bestScore.text = "Best Score: 0"
+        bestScore.fontColor = SKColor.white
+        self.addChild(bestScore)
+        
+        //Create play button
+        playButton = SKShapeNode()
+        playButton.name = "play_button"
+        playButton.zPosition = 1
+        playButton.position = CGPoint(x: 0, y: (frame.size.height / -2) + 200)
+        playButton.fillColor = SKColor.cyan
+        
+        let topCorner = CGPoint(x: -50, y: 50)
+        let bottonCorner = CGPoint(x: -50, y: -50)
+        let middle = CGPoint(x: 50, y: 0)
+        let path = CGMutablePath()
+        path.addLine(to: topCorner)
+        path.addLines(between: [topCorner, bottonCorner, middle])
+        playButton.path = path
+        self.addChild(playButton)
+    }
+    
+    private func startGame() {
+        print("Game started")
+        
+        gameLogo.run(SKAction.move(by: CGVector(dx: 0, dy: 600), duration: 0.7)) {
+            self.gameLogo.isHidden = true
+        }
+        
+        playButton.run(SKAction.scale(to: CGFloat(0), duration: TimeInterval(0.7))) {
+            self.playButton.isHidden = true
+        }
+        
+        let bottomCorner = CGPoint(x: 0, y: (frame.size.height / -2) + 20)
+        bestScore.run(SKAction.move(to: bottomCorner, duration: 0.7)) {
+            self.gameBG.setScale(0)
+            self.currentScore.setScale(0)
+            self.gameBG.isHidden = false
+            self.currentScore.isHidden = false
+            
+            self.gameBG.run(SKAction.scale(to: 1, duration: 0.4))
+            self.currentScore.run(SKAction.scale(to: 1, duration: 0.4))
+            
+            self.game.initGame()
+        }
+    }
+    
+    private func initGameView() {
+        //Add currentScore label
+        currentScore = SKLabelNode(fontNamed: "ArialRoundedMTBold")
+        currentScore.zPosition = 1
+        currentScore.position = CGPoint(x: 0, y: (frame.size.height / -2) + 60)
+        currentScore.fontSize = 40
+        currentScore.fontColor = SKColor.white
+        currentScore.isHidden = true
+        currentScore.text = "Score: 0"
+        self.addChild(currentScore)
+        
+        //Add game board
+        let width = 550
+        let heigth = 1100
+        let rect = CGRect(x: -width / 2, y: -heigth / 2, width: width, height: heigth)
+        gameBG = SKShapeNode(rect: rect, cornerRadius: 0.02)
+        gameBG.fillColor = SKColor.darkGray
+        gameBG.zPosition = 2
+        gameBG.isHidden = true
+        self.addChild(gameBG)
+        
+        createGameBoard(width: Int(width), heigth: Int(heigth))
+    }
+    
+    private func createGameBoard(width: Int, heigth: Int) {
+        let cellWidth: CGFloat = 27.5
+        let numRows = 40
+        let numCols = 20
+        var x = CGFloat(width / -2) + (cellWidth / 2)
+        var y = CGFloat(heigth / 2) - (cellWidth / 2)
+        
+        for i in 0...numRows - 1 {
+            for j in 0...numCols - 1 {
+                let cellNode = SKShapeNode(rectOf: CGSize(width: cellWidth, height: cellWidth))
+                cellNode.strokeColor = SKColor.black
+                cellNode.zPosition = 2
+                cellNode.position = CGPoint(x: x, y: y)
+                gameArray.append((node: cellNode, x: i, y: j))
+                gameBG.addChild(cellNode)
+                
+                x += cellWidth
+            }
+            //reset x, iterate y
+            x = CGFloat(width / -2) + (cellWidth / 2)
+            y -= cellWidth
+        }
+        ;
+    }
+    
 }
